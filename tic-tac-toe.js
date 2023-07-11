322d7ee4f63a4c84f3be358495e6acf0643e8acd
Joe Chek <jmchek@gmail.com>
2023-07-11 15:42:13 -0700

    create skeleton for js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,28 @@
-console.log("hello");
+
+//gameboard is a module
+const gameboard = (() => {
+    let gboardArr = [];
+
+    return console.log("gameboard works");
+  })();
+
+
+//playerMaker is a factory
+const playerMaker = (name, age) => {
+    const sayHello = () => console.log(name + ' says: "playerMaker works"');
+    return { name, age, sayHello };
+};
+
+const jeff = playerMaker('jeff', 27);
+
+console.log(jeff.name); // 'jeff'
+
+jeff.sayHello();
+
+
+//gameController is a module
+const gameController = (() => {
+    let gboardArr = [];
+
+    return console.log("gameController works");
+  })();
