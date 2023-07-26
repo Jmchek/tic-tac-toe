2280a833dbd4e9d0ef58d202fa9896308ee39b30
Joe Chek <jmchek@gmail.com>
2023-07-26 14:26:25 -0700

    add grid, add player starting button to determine 'x' or 'o', and add click event reader for tiles

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tic-tac-toe
-
-1. create a grid to house the x's and o's
+1. user clicks on either x or o to determine game
+2. user clicks on a section tile which sends event
+3. event is sent out with event info detailing which block it is
+4. block is then filled in with choice from module
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,40 +1,46 @@
-//need to setup the board with html first
-// then we can tie events to each section, so row 1 col 1 etc
+//make fillBoardX work with new gameboard tileGrabber
 
 //gameboard is a module
 const gameboard = (() => {
     let gboardArrX = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
-    let gboardArrO = ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'];
-    let playArea = document.querySelector('.gboard');
-    let playAreaX = document.querySelector('#xDump');
-    let playAreaO = document.querySelector('#oDump');
-    let xFill = "";
-    let oFill = "";
+    // let gboardArrO = ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'];
+    // let playArea = document.querySelector('.gboard');
+    // let playAreaX = document.querySelector('#xDump');
+    // let playAreaO = document.querySelector('#oDump');
+    // let xFill = "";
+    // let oFill = "";
+    let gameInSession = false;
+    let toggleForm = "X";
+
+    //tiles
+    let tileGrabber = document.querySelectorAll('.tiles');
+
+    tileGrabber.forEach((tile, index) => {
+      tile.addEventListener('click', () => {
+        //if toggle form for game is 'x'
+        //fill the tile matching id with 'x'
+        // console.log(`${index}: ${tile}`);
+        toggleForm == "X" ? fillBoardX(index) : fillBoardO();
+      });
+    });
     
 
-    function fillBoardX() {
-      let i = 0;
-
-      while (i < gboardArrX.length) {
-        xFill += gboardArrX[i];
-        i++;
-      }
-
-      playAreaX.innerText = xFill;
+    function fillBoardX(someTileIndex) {
+      tileGrabber[someTileIndex].innerText = "hello there";
     }
 
     function fillBoardO() {
-      let i = 0;
+      // let i = 0;
 
-      while (i < gboardArrO.length) {
-        oFill += gboardArrO[i];
-        i++;
-      }
+      // while (i < gboardArrO.length) {
+      //   oFill += gboardArrO[i];
+      //   i++;
+      // }
 
-      playAreaO.innerText = oFill;
+      // playAreaO.innerText = oFill;
     }
 
-    return fillBoardX(), fillBoardO();
+    return fillBoardX, fillBoardO();
   })();
 
 
